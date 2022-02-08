c2ee2e994c4275269ebb099559b1b394a5d56c95
mai.nguyen-tiki <mai.nguyen6@tiki.vn>
2022-02-08 12:50:54 +0700

    feat: add get all contacts

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -45,7 +45,7 @@ declare type TboxCommonsType = {
 declare type TboxContactsType = {
     openContactForm(options: AddPhoneContact): Promise<Array<number>>;
     addToExistingContact(option: AddPhoneContact): Promise<Array<number>>;
-    getAllScope(options?: Array<any>): Promise<Array<number>>;
+    getAllScope(): Promise<Array<number>>;
 };
 declare const TboxContacts: TboxContactsType;
 declare const _default: TboxCommonsType;
